ecab3258302673a11978626bc11b241c2a1d1dfc
Rafael Schloming <rhs@alum.mit.edu>
2017-06-21 16:35:25 -0400

    factored out logging, task control, and display into a tasks library and added tests

---
+++ forge/tasks.py
@@ -0,0 +1,437 @@
+# Copyright 2017 datawire. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import eventlet
+from eventlet.corolocal import local
+
+class Dummy(object):
+
+    def __getattr__(self, attr):
+        raise Exception("The tasks module has not been initialized yet. The tasks module depends on eventlet which requires monkeypatching. Please invoke tasks.setup() at the earliest possible point in your code.")
+
+logging = sys = time = traceback = blessed = Dummy()
+
+def setup():
+    """
+    Setup the task system. This will perform eventlet monkey patching as well as set up logging.
+    """
+
+    eventlet.sleep() # workaround for import cycle: https://github.com/eventlet/eventlet/issues/401
+    eventlet.monkey_patch()
+
+    global logging, sys, time, traceback, blessed
+    import logging, sys, time, traceback, blessed
+
+    if 'pytest' not in sys.modules:
+        import getpass
+        getpass.os = eventlet.patcher.original('os') # workaround for https://github.com/eventlet/eventlet/issues/340
+
+    logging.getLogger("tasks").addFilter(TaskFilter())
+    logging.basicConfig(level=logging.INFO, format='%(levelname)s %(task_id)s: %(message)s')
+
+class Sentinel(object):
+
+    """A convenience class that can be used for creating constant values that str/repr using their constant name."""
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return self.name
+
+"""A sentinal value used to indicate that the task is not yet complete."""
+PENDING = Sentinel("PENDING")
+
+"""A sentinal value used to indicate that the task terminated with an error of some kind."""
+ERROR = Sentinel("ERROR")
+
+def elapsed(delta):
+    """
+    Return a pretty representation of an elapsed time.
+    """
+    minutes, seconds = divmod(delta, 60)
+    hours, minutes = divmod(minutes, 60)
+    return "%d:%02d:%02d" % (hours, minutes, seconds)
+
+class TaskFilter(object):
+
+    """
+    This logging filter augments log records with useful context when
+    log statements are made within a task. It also captures the log
+    messages made within a task and records them in the execution
+    object for a task invocation.
+
+    """
+
+    def filter(self, record):
+        exe = execution.current()
+        record.task_id = exe.id
+        exe.log_record(record)
+        return False
+
+class task(object):
+
+    """A decorator used to mark a given function or method as a task.
+
+    A task can really be any python code, however it is expected that
+    tasks will perform scripting, coordination, integration, and
+    general glue-like activities that are used to automate tasks on
+    behalf of humans.
+
+    This kind of code generally suffers from a number of problems:
+
+     - There is rarely good user feedback for what is happening at any
+       given moment.
+
+     - When integration assumptions are violated (e.g. remote system
+       barfs) the errors are often swallowed/opaque.
+
+     - Because of the way it is incrementally built via growing
+       convenience scripts it is often opaque and difficult to debug.
+
+     - When parallel workflows are needed, they are difficult to code
+       in a way that preserves clear user feedback on progress and
+       errors.
+
+    Using the task decorator provides a number of conveniences useful
+    for this kind of code.
+
+     - Task arguments/results are automatically captured for easy
+       debugging.
+
+     - Convenience APIs for status updates and progress indicators
+       allow tasks to trivially provide good user feedback.
+
+     - Convenience APIs for executing tasks in parallel.
+
+     - Convenience for safely executing shell and http requests with
+       good error reporting and user feedback.
+
+    Any python function can be marked as a task and invoked in the
+    normal way you would invoke any function, e.g.:
+
+        @task("Normalize Path")
+        def normpath(path):
+            status("splitting path: %s" % path)
+            parts = [p for p in path.split("/") if p]
+            status("filtered path: %s" % ", ".join(parts))
+            normalized = "/".join(parts)
+            if path.startswith("/"):
+              return "/" + normalized
+            else:
+              return normalized
+
+        print normpath("/foo//bar/baz") -> "/foo/bar/baz"
+
+    The decorator however provides several other convenient ways you
+    can invoke a task:
+
+        # using normpath.go, I can launch subtasks in parallel
+        normalized = normpath.go("asdf"), normpath.go("fdsa"), normpath.go("bleh")
+        # now I can fetch the result of an individual subtask:
+        result = normalized[0].get()
+        # or sync on any outstanding sub tasks:
+        sync()
+
+    You can also run a task. This will render progress indicators,
+    status, and errors to the screen as the task and any subtasks
+    proceed:
+
+        normpath.run("/foo//bar/baz")
+
+    """
+
+    def __init__(self, name = None):
+        self.name = name
+        self.logger = logging.getLogger("tasks")
+        self.count = 0
+
+    def generate_id(self):
+        self.count += 1
+        return self.count
+
+    def __call__(self, function):
+        self.function = function
+        if self.name is None:
+            self.name = self.function.__name__
+        return decorator(self)
+
+class decorator(object):
+
+    def __init__(self, task):
+        self.task = task
+
+    def __call__(self, *args, **kwargs):
+        return execution.call(self.task, args, kwargs)
+
+    def go(self, *args, **kwargs):
+        return execution.spawn(self.task, args, kwargs)
+
+    def run(self, *args, **kwargs):
+        terminal = blessed.Terminal()
+        moved = 0
+
+        exe = self.go(*args, **kwargs)
+
+        for _ in exe.events:
+            lines = exe.render().splitlines()
+            screenful = lines[-terminal.height:]
+
+            sys.stdout.write(terminal.move_up*moved)
+
+            for idx, line in enumerate(reversed(screenful)):
+                # XXX: should really wrap this properly somehow, but
+                #      writing out more than the terminal width will mess up
+                #      the movement logic
+                delta = len(line) - terminal.length(line)
+                sys.stdout.write(line[:terminal.width+delta])
+                sys.stdout.write(terminal.clear_eol + terminal.move_down)
+
+            sys.stdout.write(terminal.clear_eol)
+            moved = len(screenful)
+
+class execution(object):
+
+    CURRENT = local()
+
+    @classmethod
+    def current(cls):
+        return getattr(cls.CURRENT, "execution", None)
+
+    @classmethod
+    def set(cls, value):
+        cls.CURRENT.execution = value
+
+    def __init__(self, task, args, kwargs):
+        self.task = task
+        self.parent = self.current()
+        self.args = args
+        self.kwargs = kwargs
+        self.children = []
+        if self.parent is not None:
+            self.parent.children.append(self)
+            self.thread = self.parent.thread
+        else:
+            self.thread = None
+
+        self._dirty_events = set()
+
+        # start time
+        self.started = None
+        # capture the log records emitted during execution
+        self.records = []
+        # capture the current status
+        self.status = None
+        # end time
+        self.finished = None
+
+        # the return result
+        self.result = PENDING
+        # any exception that was produced
+        self.exception = None
+
+        # outstanding child tasks
+        self.outstanding = []
+
+        if self.parent is None:
+            self.index = self.task.generate_id()
+        else:
+            self.index = len([c for c in self.parent.children if c.task == task])
+
+        self.id = ".".join("%s[%s]" % (e.task.name, e.index) for e in self.stack)
+
+    @property
+    def traversal(self):
+        yield self
+        for c in self.children:
+            for d in c.traversal:
+                yield d
+
+    def fire(self, event):
+        self._dirty_events.add(event)
+        eventlet.sleep()
+
+    def _pop_events(self):
+        result = []
+        for e in self.traversal:
+            if e._dirty_events:
+                popped = list(e._dirty_events)
+                popped.sort()
+                e._dirty_events.clear()
+                for evt in popped:
+                    result.append((e, evt))
+        return result
+
+    @property
+    def events(self):
+        while True:
+            events = self._pop_events()
+            if not events and not self.running:
+                return
+            yield events
+            eventlet.sleep()
+
+    def update_status(self, message):
+        self.status = message
+        self.fire("status")
+        self.info(message)
+
+    def log_record(self, record):
+        self.records.append(record)
+        self.fire("record")
+
+    def log(self, *args, **kwargs):
+        self.task.logger.log(*args, **kwargs)
+
+    def info(self, *args, **kwargs):
+        self.task.logger.info(*args, **kwargs)
+
+    @property
+    def running(self):
+        return self.result is PENDING or self.outstanding
+
+    @classmethod
+    def call(cls, task, args, kwargs):
+        exe = execution(task, args, kwargs)
+        exe.run()
+        return exe.get()
+
+    @classmethod
+    def spawn(cls, task, args, kwargs):
+        exe = execution(task, args, kwargs)
+        exe.thread = eventlet.spawn(exe.run)
+        if exe.parent:
+            exe.parent.outstanding.append(exe)
+        return exe
+
+    @property
+    def stack(self):
+        result = []
+        exe = self
+        while exe:
+            result.append(exe)
+            exe = exe.parent
+        result.reverse()
+        return result
+
+    def enter(self):
+        args = [str(a) for a in self.args]
+        args.extend("%s=%s" % (k, v) for k, v in self.kwargs.items())
+
+        self.info("START(%s)" % ", ".join(args))
+
+    def exit(self):
+        self.info("RESULT -> %s (%s)" % (self.result, elapsed(self.finished - self.started)))
+
+    def run(self):
+        self.set(self)
+        self.started = time.time()
+        self.enter()
+        try:
+            self.result = self.task.function(*self.args, **self.kwargs)
+        except:
+            self.result
+            self.exception = sys.exc_info()
+            self.result = ERROR
+        finally:
+            self.sync()
+            self.finished = time.time()
+            self.exit()
+            self.set(self.parent)
+
+    def sync(self):
+        result = []
+        while self.outstanding:
+            ch = self.outstanding.pop(0)
+            ch.thread.wait()
+            result.append(ch)
+        return result
+
+    def wait(self):
+        if self.thread != None and self.result is PENDING:
+            self.thread.wait()
+
+    def get(self):
+        self.wait()
+        if self.result is ERROR:
+            raise self.exception[0], self.exception[1], self.exception[2]
+        else:
+            return self.result
+
+    @property
+    def indent(self):
+        return "  "*(len(self.stack) - 1)
+
+    @property
+    def error_summary(self):
+        return "".join(traceback.format_exception_only(*self.exception[:2])).strip()
+
+    @property
+    def render_line(self):
+        summary = self.status or "(in progress)" if self.result is PENDING else \
+                  "(error) %s" % self.error_summary if self.result is ERROR else \
+                  str(self.result) if self.result is not None else \
+                  ""
+
+        result = "%s%s: %s" % (self.indent, self.task.name, summary)
+        if self.result == ERROR and (self.parent is None or self.thread != self.parent.thread):
+            indent = "\n  " + self.indent
+            exc = "".join(traceback.format_exception(*self.exception))
+            result += "\n" + indent + exc.replace("\n", indent)
+        return result
+
+    def render(self):
+        return "\n".join([e.render_line for e in self.traversal])
+
+def sync():
+    """
+    Wait until all child tasks have terminated.
+    """
+    return execution.current().sync()
+
+def log(*args, **kwargs):
+    """
+    Log a message for the current task.
+    """
+    execution.current().log(*args, **kwargs)
+
+def info(*args, **kwargs):
+    """
+    Log an info message for the current task.
+    """
+    execution.current().info(*args, **kwargs)
+
+def debug(*args, **kwargs):
+    """
+    Log a debug message for the current task.
+    """
+    execution.current().debug(*args, **kwargs)
+
+def warn(*args, **kwargs):
+    """
+    Log a warn message for the current task.
+    """
+    execution.current().warn(*args, **kwargs)
+
+def error(*args, **kwargs):
+    """
+    Log an error message for the current task.
+    """
+    execution.current().error(*args, **kwargs)
+
+def status(*args, **kwargs):
+    """
+    Update the status for the current task. This will log an info message with the new status.
+    """
+    return execution.current().update_status(*args, **kwargs)
---
+++ forge/tests/test_tasks.py
@@ -0,0 +1,173 @@
+# Copyright 2017 datawire. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from forge.tasks import execution, setup, status, sync, task, PENDING, ERROR
+
+setup()
+
+import time
+
+# used to check success cases
+@task("Noop")
+def noop(x):
+    return x
+
+# used to check failure cases
+@task("Oops")
+def oops(x):
+    return x/0
+
+def test_success_sync():
+    assert noop(3) == 3
+
+def test_success_async():
+    exe = noop.go(3)
+    assert exe.result == PENDING
+    exe.wait()
+    assert exe.result == 3
+    assert exe.exception is None
+    assert exe.get() == 3
+
+def test_failure_sync():
+    try:
+        oops(3)
+        assert False, "should have failed"
+    except ZeroDivisionError, e:
+        pass
+
+def test_failure_async():
+    exe = oops.go(3)
+    assert exe.result == PENDING
+    exe.wait()
+    assert exe.result == ERROR
+    assert exe.exception
+    assert exe.exception[0] == ZeroDivisionError
+    try:
+        exe.get()
+        assert False, "should have failed"
+    except ZeroDivisionError, e:
+        pass
+
+# used to check sync
+@task("Scatter")
+def scatter(n):
+    results = [noop.go(i) for i in range(n)]
+    sync()
+    return [r.result for r in results]
+
+def test_sync():
+    gathered = scatter(10)
+    for i, x in enumerate(gathered):
+        assert i == x
+
+@task()
+def steps():
+    status("step 1")
+    time.sleep(0.1)
+    status("step 2")
+    time.sleep(0.1)
+    status("step 3")
+
+def test_status():
+    exe = steps.go()
+    statuses = []
+    for events in exe.events:
+        for e, evt in events:
+            if evt == "status":
+                statuses.append(e.status)
+    assert statuses == ["step 1", "step 2", "step 3"]
+
+def test_render():
+    gathered = scatter.go(10)
+    gathered.wait()
+    assert """Scatter: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  Noop: 0
+  Noop: 1
+  Noop: 2
+  Noop: 3
+  Noop: 4
+  Noop: 5
+  Noop: 6
+  Noop: 7
+  Noop: 8
+  Noop: 9""" == gathered.render()
+
+@task()
+def nested_oops_sync():
+    noop(1)
+    oops(2)
+    noop(3)
+
+@task()
+def nested_oops_async():
+    noop.go(1)
+    oops.go(2)
+    noop.go(3)
+
+import re
+
+# replace filename and line number references in stack traces so they
+# are less brittle/system dependend and we can assert on them
+def massage(text):
+    return re.sub(r', line [0-9]+',
+                  r', line <NNN>',
+                  re.sub(r'File "(?:([^"].*)/([^/"].*))"',
+                         r'File "<PATH>/\2"',
+                         text))
+
+def test_massage():
+    assert massage('File "fdsa.py"') == 'File "fdsa.py"'
+    assert massage('File "asdf/fdsa.py"') == 'File "<PATH>/fdsa.py"'
+    assert massage('File "/asdf/fdsa.py"') == 'File "<PATH>/fdsa.py"'
+    assert massage('File "/blah/asdf/fdsa.py"') == 'File "<PATH>/fdsa.py"'
+    assert massage('File "asdf.py", line 123') == 'File "asdf.py", line <NNN>'
+
+def test_nested_exception_sync():
+    exe = nested_oops_sync.go()
+    exe.wait()
+    assert """nested_oops_sync: (error) ZeroDivisionError: integer division or modulo by zero
+
+  Traceback (most recent call last):
+    File "<PATH>/tasks.py", line <NNN>, in run
+      self.result = self.task.function(*self.args, **self.kwargs)
+    File "<PATH>/test_tasks.py", line <NNN>, in nested_oops_sync
+      oops(2)
+    File "<PATH>/tasks.py", line <NNN>, in __call__
+      return execution.call(self.task, args, kwargs)
+    File "<PATH>/tasks.py", line <NNN>, in call
+      return exe.get()
+    File "<PATH>/tasks.py", line <NNN>, in run
+      self.result = self.task.function(*self.args, **self.kwargs)
+    File "<PATH>/test_tasks.py", line <NNN>, in oops
+      return x/0
+  ZeroDivisionError: integer division or modulo by zero
+  
+  Noop: 1
+  Oops: (error) ZeroDivisionError: integer division or modulo by zero""" == massage(exe.render())
+
+def test_nested_exception_async():
+    exe = nested_oops_async.go()
+    exe.wait()
+    assert """nested_oops_async: 
+  Noop: 1
+  Oops: (error) ZeroDivisionError: integer division or modulo by zero
+
+    Traceback (most recent call last):
+      File "<PATH>/tasks.py", line <NNN>, in run
+        self.result = self.task.function(*self.args, **self.kwargs)
+      File "<PATH>/test_tasks.py", line <NNN>, in oops
+        return x/0
+    ZeroDivisionError: integer division or modulo by zero
+    
+  Noop: 3""" == massage(exe.render())
