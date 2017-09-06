abb8ac5966dd8d0dfb022840fa3d2b8fe343f0d3
Philip Lombardi <plombardi@datawire.io>
2017-09-06 14:52:50 -0400

    implement scout

--- forge/cli.py
+++ forge/cli.py
@@ -67,6 +67,10 @@ from .kubernetes import Kubernetes
 from .jinja2 import renders
 from .istio import istio
 from .output import Terminal
+from scout import Scout
+
+scout = Scout("forge", __version__)
+scout_res = scout.report()
 
 ENV = find_dotenv(usecwd=True)
 if ENV: load_dotenv(ENV)
