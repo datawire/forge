3324a06130395333404a4919cff18d22c40ff121
Rafael Schloming <rhs@alum.mit.edu>
2017-10-17 10:15:35 -0400

    updated to use x-download

--- devcurl.sh
+++ devcurl.sh
@@ -5,6 +5,6 @@ TARGET="$1"
 if [ -z "${TARGET}" ]; then
     echo "usage: devcurl.sh <output-file>"
 else
-    curl https://s3.amazonaws.com/datawire-static-files/forge/$(curl https://s3.amazonaws.com/datawire-static-files/forge/latest.url?x-downloaded=datawire)/forge?x-downloaded=datawire -o "${TARGET}"
+    curl https://s3.amazonaws.com/datawire-static-files/forge/$(curl https://s3.amazonaws.com/datawire-static-files/forge/latest.url?x-download=datawire)/forge?x-download=datawire -o "${TARGET}"
     chmod a+x "${TARGET}"
 fi
