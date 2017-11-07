314f62f9ff7b498374016c91bb172680c2db4397
Rafael Schloming <rhs@alum.mit.edu>
2017-11-07 10:34:45 -0500

    made setup support ecr, gcr, and generic registry options

--- forge/cli.py
+++ forge/cli.py
@@ -73,10 +73,7 @@ class CLIError(Exception): pass
 
 SETUP_TEMPLATE = """# Global forge configuration
 # DO NOT CHECK INTO GITHUB, THIS FILE CONTAINS SECRETS
-docker-repo: {{docker}}
-user: {{user}}
-password: >
-  {{password}}
+{{yaml}}
 """
 
 def file_contents(path):
@@ -103,15 +100,19 @@ class Forge(object):
         self.rendered = []
         self.deployed = []
 
-    def prompt(self, msg, default=None, loader=None, echo=True):
+    def prompt(self, msg, default=None, loader=None, echo=True, optional=False):
+        if optional:
+            msg += ' (use "-" to leave unspecified)'
         prompt = "%s: " % msg if default is None else "%s[%s]: " % (msg, default)
-        prompter = raw_input if echo else getpass.getpass
+        prompter = raw_input if echo else lambda: getpass.getpass("")
 
         while True:
-            task.echo(newline=False)
+            task.echo(prompt, newline=False)
             sys.stdout.flush()
-            value = prompter(prompt) or default
+            value = prompter() or default
             if value is None: continue
+            if value == "-" and optional:
+                value = None
             if loader is not None:
                 loaded = loader(value)
                 if loaded is None:
@@ -143,15 +144,21 @@ class Forge(object):
                     task.echo(self.terminal.bold("Please make sure kubectl is installed/configured correctly."))
                     raise CLIError("")
 
-            registry = "registry.hub.docker.com"
-            repo = None
-            user = os.environ.get("USER", "")
-            password = None
-            json_key = None
+            regtype = "generic"
+            prompts = {
+                ("generic", "url"): ("Docker registry url", "registry.hub.docker.com"),
+                ("generic", "user"): ("Docker user", None),
+                ("generic", "namespace"): ("Docker namespace/organization", None),
+                ("generic", "password"): ("Docker password", None),
+                ("gcr", "key"): ["Path to json key", None]
+            }
 
             @task()
             def validate():
-                dr = Docker(registry, repo, user, password)
+                c = yaml.dump({"registry": regvalues})
+                task.echo(c)
+                conf = config.load("setup", c)
+                dr = get_docker(conf.registry)
                 dr.validate()
 
             task.echo()
@@ -159,13 +166,37 @@ class Forge(object):
 
             while True:
                 task.echo()
-                registry = self.prompt("Docker registry", registry)
-                user = self.prompt("Docker user", user)
-                repo = self.prompt("Docker organization", user)
-                if user == "_json_key":
-                    json_key, password = self.prompt("Path to json key", json_key, loader=file_contents)
-                else:
-                    password = self.prompt("Docker password", echo=False)
+                types = OrderedDict((("ecr", config.ECR),
+                                     ("gcr", config.GCR),
+                                     ("generic", config.DOCKER)))
+                regtype = self.prompt("Registry type (one of %s)" % ", ".join(types.keys()), regtype)
+                if regtype not in types:
+                    task.echo()
+                    task.echo(
+                        self.terminal.red("%s is not a valid choice, please choose one of %s" %
+                                          (regtype, ", ".join(types.keys())))
+                    )
+                    task.echo()
+                    regtype = "generic"
+                    continue
+
+                reg = types[regtype]
+                regvalues = OrderedDict((("type", reg.fields["type"].type.value),))
+                for f in reg.fields.values():
+                    if f.name == "type": continue
+                    prompt, default = prompts.get((regtype, f.name), (f.name, None))
+                    if (regtype, f.name) == ("gcr", "key"):
+                        key, value = self.prompt(prompt, default, loader=file_contents)
+                        prompts[(regtype, f.name)][1] = key
+                    else:
+                        if f.name in ("password",):
+                            value = self.prompt(prompt, default, echo=False)
+                        else:
+                            value = self.prompt(prompt, default, optional=not f.required)
+                    if f.name in ("password", "key"):
+                        regvalues[f.name] = base64.encodestring(value)
+                    else:
+                        regvalues[f.name] = value
 
                 task.echo()
                 e = validate.run()
@@ -179,20 +210,19 @@ class Forge(object):
 
             task.echo()
 
-            config = renders("SETUP_TEMPLATE", SETUP_TEMPLATE,
-                             docker="%s/%s" % (registry, repo),
-                             user=user,
-                             password=base64.encodestring(password).replace("\n", "\n  "))
+            config_content = renders("SETUP_TEMPLATE", SETUP_TEMPLATE,
+                                     yaml=yaml.dump({"registry": regvalues}, allow_unicode=True,
+                                                    default_flow_style=False))
 
             config_file = "forge.yaml"
 
             task.echo(self.terminal.bold("== Writing config to %s ==" % config_file))
 
             with open(config_file, "write") as fd:
-                fd.write(config)
+                fd.write(config_content)
 
             task.echo()
-            task.echo(config.strip())
+            task.echo(config_content.strip())
             task.echo()
 
             task.echo(self.terminal.bold("== Done =="))
--- forge/tests/blackbox/test_blackbox.py
+++ forge/tests/blackbox/test_blackbox.py
@@ -1,6 +1,4 @@
-import os, pytest
-
-from forge.tasks import sh
+import os, pexpect, pytest, sys
 
 DIR = os.path.dirname(__file__)
 EXCLUDES = ("__pycache__",)
@@ -8,5 +6,35 @@ EXCLUDES = ("__pycache__",)
 @pytest.mark.parametrize("directory", [n for n in os.listdir(DIR)
                                        if os.path.isdir(os.path.join(DIR, n)) and n not in EXCLUDES])
 def test(directory):
-    output = sh("forge", "deploy", cwd=os.path.join(DIR, directory))
-    assert output.code == 0
+    print
+
+    test_dir = os.path.join(DIR, directory)
+    test_spec = os.path.join(test_dir, "test.spec")
+
+    if os.path.exists(test_spec):
+        with open(test_spec) as fd:
+            ops = fd.read()
+    else:
+        ops = "RUN forge deploy"
+
+    child = None
+
+    for line in ops.splitlines():
+        for stmt in line.split(";"):
+            op, arg = stmt.split(None, 1)
+            if op == "RUN":
+                child = pexpect.spawn(arg, cwd=os.path.join(DIR, directory))
+                child.logfile = sys.stdout
+            elif op == "OUT":
+                child.expect_exact(arg.strip())
+            elif op == "TYPE":
+                if arg.strip().lower() == "<enter>":
+                    child.sendline()
+                else:
+                    child.sendline(arg)
+            else:
+                assert False, "unrecognized op: %s" % op
+
+    if child is not None:
+        child.expect(pexpect.EOF)
+        assert child.wait() == 0
